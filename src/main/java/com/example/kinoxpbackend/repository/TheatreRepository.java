36fca267965437b2d23888aa4d1de4873a292c87
KvasirSG <12102444+KvasirSG@users.noreply.github.com>
2025-03-06 10:20:55 +0000

    Added Theatre entity, repository, service, and controller

---
+++ src/main/java/com/example/kinoxpbackend/repository/TheatreRepository.java
@@ -0,0 +1,8 @@
+package com.example.kinoxpbackend.repository;
+
+import com.example.kinoxpbackend.model.Theatre;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TheatreRepository extends JpaRepository<Theatre, Long> {
+}
+
