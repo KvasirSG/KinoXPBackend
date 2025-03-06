aaffb4c7ebbd83d2dcfdeda9e93dd2e04d1e2596
KvasirSG <12102444+KvasirSG@users.noreply.github.com>
2025-03-06 10:29:47 +0000

    Added Movie entity, repository, service, and controller

---
+++ src/main/java/com/example/kinoxpbackend/service/MovieService.java
@@ -0,0 +1,34 @@
+package com.example.kinoxpbackend.service;
+
+import com.example.kinoxpbackend.model.Movie;
+import com.example.kinoxpbackend.repository.MovieRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+
+    private final MovieRepository movieRepository;
+
+    public MovieService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    public List<Movie> getAllMovies() {
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> getMovieById(Long id) {
+        return movieRepository.findById(id);
+    }
+
+    public Movie createMovie(Movie movie) {
+        return movieRepository.save(movie);
+    }
+
+    public void deleteMovie(Long id) {
+        movieRepository.deleteById(id);
+    }
+}
+
